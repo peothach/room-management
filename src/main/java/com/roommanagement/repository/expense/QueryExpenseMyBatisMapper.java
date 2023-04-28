5061692f959898ba974d9c18a9337bd6e6d18b04
ThachNH3 <ThachNh3>
2023-04-28 17:24:42 +0700

    Update query statement.

--- src/main/java/com/roommanagement/repository/expense/QueryExpenseMyBatisMapper.java
+++ src/main/java/com/roommanagement/repository/expense/QueryExpenseMyBatisMapper.java
@@ -21,8 +21,8 @@ public interface QueryExpenseMyBatisMapper {
       "      LEFT JOIN unit_price u ON u.unit_price_id = e.unit_price_id \n" +
       "      JOIN users on users.id = e.user_id \n" +
       "      WHERE users.id = #{userId}\n" +
-      "      AND r.id IS NULL OR r.status <> 'Phòng đã xóa'\n" +
-      "      AND re.room_expense_id IS NULL OR re.override_price_flag IS FALSE\n" +
+      "      AND (r.id IS NULL OR r.status <> 'Phòng đã xóa')\n" +
+      "      AND (re.room_expense_id IS NULL OR re.override_price_flag IS FALSE)\n" +
       "      ORDER BY e.expense_id")
   @Results(id = "expensesResultMap", value = {
       @Result(property = "expenseId", column = "expense_id"),
